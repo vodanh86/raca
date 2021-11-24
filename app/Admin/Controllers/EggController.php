c932a721cd56b0195b9454241796729566edcf7e
Manh Cuong <cuong@radioactive.sg>
2021-11-24 13:14:41 +0800

    add egg controller

--- app/Admin/Controllers/EggController.php
+++ app/Admin/Controllers/EggController.php
@@ -17,6 +17,16 @@ class EggController extends AdminController
      */
     protected $title = 'Egg';
 
+    protected function getPrice(){
+        $url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=radio-caca';
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HTTPGET, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response_json = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($response_json, true)[0]["current_price"];
+    }
+
     /**
      * Make a grid builder.
      *
@@ -24,18 +34,29 @@ class EggController extends AdminController
      */
     protected function grid()
     {
+        $price = $this->getPrice();
         $grid = new Grid(new Egg());
-
-        $grid->column('id', __('Id'));
-        $grid->column('highest_price', __('Highest price'));
-        $grid->column('count', __('Count'));
-        $grid->column('fixed_price', __('Fixed price'));
-        $grid->column('image_url', __('Image url'));
         $grid->column('name', __('Name'));
-        $grid->column('total_price', __('Total price'));
+        $grid->column('image_url', __('Image url'))->image("",50,50);
+        $grid->id('Mua')->display(function($item){
+            return "<span class='label label-success'><a target='_blank' href='https://market.radiocaca.com/#/market-place/".$item."' style='color:white'>Mua</a></span>";
+        });
+        $grid->column('count', __('Count'))->sortable()->filter('range');
+        $grid->column('fixed_price', __('Price (RACA)'))->display(function ($title) {
+            return number_format($title, 0);
+        })->sortable()->filter('range');
+        $grid->column('highest_price', __('Price (USD)'))->display(function () use($price) {
+            return number_format($price * $this->fixed_price, 0)." $";
+        })->sortable()->filter('range');
+        $grid->column('highest_price', __('Highest price'));
         $grid->column('status', __('Status'));
-        $grid->column('token_id', __('Token id'));
 
+        $grid->disableCreateButton();
+        $grid->actions(function ($actions) {
+            $actions->disableDelete();
+            $actions->disableEdit();
+        });
+        $grid->model()->orderBy('fixed_price');
         return $grid;
     }
 
