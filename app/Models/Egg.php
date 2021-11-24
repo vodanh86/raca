dc7b2425dfa458d85462b27ff97addc1884d4a4b
Manh Cuong <cuong@radioactive.sg>
2021-11-24 12:58:24 +0800

    add model

---
+++ app/Models/Egg.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Egg extends Model
+{
+    protected $table = 'egg';
+
+	protected $hidden = [
+    ];
+
+	protected $guarded = [];
+}
